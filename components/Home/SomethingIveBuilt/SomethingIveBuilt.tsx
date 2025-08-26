9ce4a3eaf4b7fba4e9541a6b012b70c0a13f457b
Thamjid M <thamjid@aqaryint.com>
2025-08-26 12:15:18 +0400

    :memo: add new contents

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -28,7 +28,96 @@ export default function SomethingIveBuilt() {
       </div>
 
       <div className="flex flex-col   xl:space-y-36 space-y-8 md:space-y-28">
-        {/* // TODO : to here  */}
+        {/* // TODO : to here  */}        
+
+        {/* // ? Project 0 Azure Horizon */}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <a href="https://resort.thamjidthachu.dev" target={"_blank"} rel="noreferrer">
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <div className="absolute px-2 bg-AAprimary rounded  ml-4 mt-2 ">
+                <span className="text-AAsecondary text-xl">In Developing...</span>
+              </div>
+
+              <Img src={"/projects/azurehorizon.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/projects/azurehorizon-sm.jpg"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+            <div className="absolute w-full   rounded  ml-4 mt-2  text-end pr-8">
+              <div className="w-full"></div>
+              <span className="text-AAsecondary sm:text-lg bg-AAprimary px-2 py-1 rounded">In Developing...</span>
+            </div>
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end sm:space-y-3 space-y-1"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Currently Developing...</span>
+                <a href="https://resort.thamjidthachu.dev" target={"_blank"} rel="noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Azure Horizon
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                  I initiated <span className="text-AAsecondary">Azure Horizon</span> in 2021 as a{" "}
+                  <span className="text-AAsecondary">demo project</span> for my first job, which
+                  later grew into a <span className="text-AAsecondary">personal passion project</span>.
+                  Originally built with <span className="text-AAsecondary">Django templates</span>,
+                  it has since evolved into a modern stack using{" "}
+                  <span className="text-AAsecondary">Django REST Framework</span> and{" "}
+                  <span className="text-AAsecondary">Next.js</span>. The vision is to transform
+                  this platform into the official digital presence of a{" "}
+                  <span className="text-AAsecondary">real resort</span> under the same name,
+                  combining <span className="text-AAsecondary">scalability</span> and{" "}
+                  <span className="text-AAsecondary">future-ready design</span> to bring the dream
+                  of <span className="text-AAsecondary">Azure Horizon Resort</span> to life.
+                </p>
+
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Python/Django</span>
+                <span className="pr-4 z-10">Next.js</span>
+                <span className="pr-4 z-10">PostgreSQL</span>
+                <span className="pr-4 z-10">Celery</span>
+                <span className="pr-4 z-10">RabbitMQ/Redis</span>
+                <span className="pr-4 z-10">Docker</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+              <GithubIcon link="https://github.com/thamjidthachu/azure-horizon" />
+              <a href="https://resort.thamjidthachu.dev" target={"_blank"} rel="noreferrer">
+                <ExternalLink url={""} router={router} />
+              </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* // ?  Project 1 Hrms Aqary */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
           {/* Left image */}
@@ -63,7 +152,7 @@ export default function SomethingIveBuilt() {
             col-span-8 flex flex-col items-start  space-y-3 md:order-1"
             >
               <div className="flex flex-col space-y-1  z-10">
-                <span className="text-AAsecondary text-base">HRMS</span>
+                <span className="text-AAsecondary text-base">Currently Developing...</span>
                 <a href="https://www.https://people.aqaryone.com/" target="_blank" rel="noopener noreferrer">
                   <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
                     HRMS - Aqary One
